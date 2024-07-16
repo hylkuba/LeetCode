016c3a0afbe812e2dfac4f380d013a82b6b1cb48
hyljaku2DESKTOP <hyljaku2@fit.cvut.cz>
2024-07-16 12:08:23 +0200

    DetermineIfTwoStringsAreClose

---
+++ LeetCode75/HashMap_Set/DetermineIfTwoStringsAreClose.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+
+class Solution {
+public:
+    bool closeStrings(std::string word1, std::string word2) {
+        if(word1.size() != word2.size()) return false;
+
+        std::vector<int> freq1(26, 0);
+        std::vector<int> freq2(26, 0);
+
+        for (char ch : word1) {
+            freq1[ch - 'a']++;
+        }
+
+        for (char ch : word2) {
+            freq2[ch - 'a']++;
+        }
+
+        // Check if there are unique characters in one of the two strings
+        for (int i = 0; i < 26; i++) {
+            if ((freq1[i] == 0 && freq2[i] != 0) || (freq1[i] != 0 && freq2[i] == 0)) {
+                return false;
+            }
+        }
+
+        std::sort(freq1.begin(), freq1.end());
+        std::sort(freq2.begin(), freq2.end());
+
+        for (int i = 0; i < 26; i++) {
+            if (freq1[i] != freq2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
