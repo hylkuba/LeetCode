44f246fc1b0b09ea07a5f047351d6fd4b0a07c89
hyljaku2DESKTOP <hyljaku2@fit.cvut.cz>
2024-07-17 11:17:18 +0200

    OddEvenLinkedList

---
+++ LeetCode75/LinkedList/OddEvenLinkedList.cpp
@@ -0,0 +1,34 @@
+#include <queue>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        // If the list is empty or has only one node, return
+        if (head == nullptr || head->next == nullptr) return head;
+
+        ListNode* odd = head;           // Initialize odd pointer to the head of the list
+        ListNode* even = head->next;    // Initialize even pointer to the second node
+        ListNode* evenRoot = even;          // Store the start of the even list
+
+        while (even != nullptr && even->next != nullptr) {
+            // Update odd and even pointers to skip one node each
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+            odd = odd->next;
+            even = even->next;
+        }
+
+        // Connect the end of the odd list to the start of the even list
+        odd->next = evenRoot;
+
+        return head;
+    }
+};
