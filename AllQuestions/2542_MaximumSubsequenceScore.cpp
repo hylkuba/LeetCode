35bc987f664c1a98c9d737824345f72b2e473a3c
hyljaku2DESKTOP <hyljaku2@fit.cvut.cz>
2024-07-28 14:31:26 +0200

    2542 - completed

---
+++ AllQuestions/2542_MaximumSubsequenceScore.cpp
@@ -0,0 +1,94 @@
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <iostream>
+
+/**
+ * @brief You are given two 0-indexed integer arrays nums1 and nums2 of equal length n and a positive integer k. You must choose a subsequence of indices from nums1 of length k.
+
+For chosen indices i0, i1, ..., ik - 1, your score is defined as:
+
+The sum of the selected elements from nums1 multiplied with the minimum of the selected elements from nums2.
+It can defined simply as: (nums1[i0] + nums1[i1] +...+ nums1[ik - 1]) * min(nums2[i0] , nums2[i1], ... ,nums2[ik - 1]).
+Return the maximum possible score.
+
+A subsequence of indices of an array is a set that can be derived from the set {0, 1, ..., n-1} by deleting some or no elements.
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,3,3,2], nums2 = [2,1,3,4], k = 3
+Output: 12
+Explanation: 
+The four possible subsequence scores are:
+- We choose the indices 0, 1, and 2 with score = (1+3+3) * min(2,1,3) = 7.
+- We choose the indices 0, 1, and 3 with score = (1+3+2) * min(2,1,4) = 6. 
+- We choose the indices 0, 2, and 3 with score = (1+3+2) * min(2,3,4) = 12. 
+- We choose the indices 1, 2, and 3 with score = (3+3+2) * min(1,3,4) = 8.
+Therefore, we return the max score, which is 12.
+Example 2:
+
+Input: nums1 = [4,2,3,1,1], nums2 = [7,5,10,9,6], k = 1
+Output: 30
+Explanation: 
+Choosing index 2 is optimal: nums1[2] * nums2[2] = 3 * 10 = 30 is the maximum possible score.
+ 
+
+Constraints:
+
+n == nums1.length == nums2.length
+1 <= n <= 105
+0 <= nums1[i], nums2[j] <= 105
+1 <= k <= n
+ * 
+ */
+
+class Solution {
+public:
+    long long maxScore(std::vector<int>& nums1, std::vector<int>& nums2, int k) {
+
+        int n = nums1.size();
+
+        long long ans = 0;
+        long long sum = 0;
+
+        std::priority_queue<int, std::vector<int>, std::greater<>> pq;
+        std::vector<std::pair<int, int>> v;
+
+        for(int i = 0; i < n; i++) {
+            v.push_back({nums2[i], nums1[i]});
+        }
+
+        std::sort(v.begin(),v.end(), std::greater<>());
+
+        int i = 0;
+        int j = 0;
+
+        while(j < n) {
+            sum += v[j].second;
+            pq.push(v[j].second);
+
+            if(j - i + 1 < k) {
+                j++;
+            } else{
+                ans = std::max(ans, sum * v[j].first);
+                sum -= pq.top();
+                pq.pop();
+                i++;
+                j++;
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main(void) {
+    Solution sol;
+    std::vector<int> nums1 = {1,3,3,2};
+    std::vector<int> nums2 = {2,1,3,4};
+    int k = 3;
+    std::cout << sol.maxScore(nums1, nums2, k) << std::endl;
+    return 0;
+}
